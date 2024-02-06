06e161d875ffdca9776cdff40b6658472cad25a6
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-02-06 15:23:58 +0600

    Check a success api endpoint.

---
+++ request_api_test/running_api.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://zyv0q9hl1g.execute-api.us-east-2.amazonaws.com/first_stage/health'
+
+while True:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(f'Status code: {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
