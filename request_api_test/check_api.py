5165cca200f5dfd3bfdd866cf08889d1ddcfcca8
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-02-24 18:09:41 +0600

    JEAC

--- request_api_test/check_api.py
+++ request_api_test/check_api.py
@@ -1,13 +1,14 @@
 from coinbase.wallet.client import Client
 
 # Replace 'YOUR_API_KEY' and 'YOUR_API_SECRET' with your actual API key and secret
-api_key = 'EWh5SD8rX8vb6'
-api_secret = 'TLgCKhAVxsCkyuLBgL7QLj1kR15U'
+api_key = 'EWh5SIcfD8rX8vb6'
+api_secret = 'TLgCQKh0HrAVxsCkyuLBgL7QLj1kR15U'
 
 client = Client(api_key, api_secret)
 
 try:
     user = client.get_current_user()
     print("API key is valid.")
+    print(user)
 except Exception as e:
     print("API key is invalid. Error:", e)  
