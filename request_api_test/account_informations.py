290ed848a45cbc098867363f1ba3451ffd2969c6
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-02-25 01:55:03 +0600

    Create account_information.py file for checking account all details.

---
+++ request_api_test/account_informations.py
@@ -0,0 +1,37 @@
+import requests
+import hmac
+import hashlib
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ.get('API_KEY_1')
+API_SECRET = os.environ.get('API_SECRET_1')
+
+def generate_signature(timestamp, method, request_path, body=''):
+    message = f"{timestamp}{method}{request_path}{body}"
+    signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).hexdigest()
+    return signature
+
+def get_account_info():
+    timestamp = str(int(time.time()))
+    method = 'GET'
+    request_path = '/v2/accounts'  # Replace with the specific endpoint for account information
+    signature = generate_signature(timestamp, method, request_path)
+
+    headers = {
+        'CB-ACCESS-KEY': API_KEY,
+        'CB-ACCESS-SIGN': signature,
+        'CB-ACCESS-TIMESTAMP': timestamp,
+        'Content-Type': 'application/json'
+    }
+
+    url = 'https://api.coinbase.com' + request_path
+    response = requests.get(url, headers=headers)
+
+    return response.json()
+
+account_info = get_account_info()
+print(account_info)
+
--- request_api_test/check_api.py
+++ request_api_test/check_api.py
@@ -1,14 +1,20 @@
 from coinbase.wallet.client import Client
+from dotenv import load_dotenv
+import os
 
-# Replace 'YOUR_API_KEY' and 'YOUR_API_SECRET' with your actual API key and secret
-api_key = 'EWh5SIcfD8rX8vb6'
-api_secret = 'TLgCQKh0HrAVxsCkyuLBgL7QLj1kR15U'
+load_dotenv()
+
+api_key = os.environ.get('API_KEY')
+api_secret = os.environ.get('API_SECRET')
 
 client = Client(api_key, api_secret)
 
 try:
     user = client.get_current_user()
     print("API key is valid.")
+    print(user)
+
+    print(user['country'])
     print(user['country']['is_in_europe'])
 except Exception as e:
     print("API key is invalid. Error:", e)  
