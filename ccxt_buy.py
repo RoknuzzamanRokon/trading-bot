8a9e00537360a749f8bd766e44c554e799e29d3f
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-03-31 22:27:14 +0600

    Add auth.

--- ccxt_buy.py
+++ ccxt_buy.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 api_key = os.getenv('API_KEY_1')
 api_secret = os.getenv('API_SECRET_1')
+
 exchange = ccxt.coinbasepro({
     'apiKey': api_key,
     'secret': api_secret,
@@ -14,16 +15,26 @@ exchange = ccxt.coinbasepro({
 symbol = 'BTC/USD'
 type = 'market'  # Type of order
 side = 'buy'  # Buying BTC
-amount = 10# Calculate the amount of BTC to buy for $10 at the limit price
+usd_amount = 10# Calculate the amount of BTC to buy for $10 at the limit price
 
-amount = usd_amount / limit_price
+# Load the markets.
+exchange.load_markets()
+
+
+ticker = exchange.fetch_ticker(symbol)
+current_price = ticker['last']
+amount = usd_amount / current_price
+
+print(amount)
+print(current_price)
+print(ticker)
 
 # Ensure the amount of BTC is within the exchange's limits and precision
-market = exchange.market(symbol)
-amount = exchange.amount_to_precision(symbol, amount)
 
+amount = exchange.amount_to_precision(symbol, amount)
+print(amount)
 try:
-    order = exchange.create_order(symbol, type, side, amount, limit_price)
+    order = exchange.create_order(symbol, type, side, amount)
     print(order)
 except Exception as e:
     print(f"An error occurred: {e}")
